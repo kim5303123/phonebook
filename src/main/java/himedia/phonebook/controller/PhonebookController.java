10c51d43bda36056edd10db0d5c6e1ab2223cf56
kim5303123 <kim5303123@gmail.com>
2025-01-21 12:04:22 +0900

    Add Jasper

--- src/main/java/himedia/phonebook/controller/PhonebookController.java
+++ src/main/java/himedia/phonebook/controller/PhonebookController.java
@@ -3,6 +3,7 @@ package himedia.phonebook.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,9 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PhonebookController {
 	private static final Logger logger = LoggerFactory.getLogger(PhonebookController.class);
 	
-	@RequestMapping(value="/", method= RequestMethod.GET)
-	@ResponseBody
+	@GetMapping("/")	
 	public String list() {
-		return "<h1>Phonebook List</h1>";
+		return "/WEB-INF/views/phonebook/list.jsp";
 	}
 }
